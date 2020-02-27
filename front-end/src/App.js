2564fb13747ba4844364d36bcc31ccc7ff40ac6b
Gintvile Bergeryte <gintvile@Simass-iMac.local>
2020-02-27 13:40:36 +0200

    text

---
+++ front-end/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Test from './Test';
+
+function App() {
+  return (
+    <div className="Todo">
+      
+        
+        <p>
+          Todo app
+        </p>
+        
+      <Test></Test>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+function Test() {
+  return (
+    <h1>Hi, I'm Test component</h1>
+  );
+}
+
+export default Test;
