5df50fe4fde5af1d7437fb16bc8390a2e11019d4
Simonas <simonas.valkiunas@gmail.com>
2020-02-27 10:55:37 +0200

    API achema added, divided into front and back

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,6 @@ const PORT = process.env.PORT || 5000;
 const CLIENT_ADR = "http://127.0.0.1:5500";
 
 
-
-
 app.use('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", CLIENT_ADR);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -29,36 +27,52 @@ app.use(express.json());
 const uri =  "mongodb+srv://simonas:anarchija@cluster0-rmuoz.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
+//Task schema
+const Task = {
+    _name : "",
+    _description: "",
+    _due_date : "",
+}
+
 app.get('/tasks', (req,res)=>{
     client.connect(err => {
         const collection = client.db("TO_DO_DB").collection("Tasks");
-
         let tasks = collection.find();
-
         let results = [];
-
         tasks.forEach(r=>{
             results.push(r);
         }, ()=>{
-
+            res.status(200);
             res.send(results);
             res.end();
         });
-
-        // client.close();
     });
 });
 
 app.post('/tasks',(req,res)=>{
     client.connect(err => {
         const collection = client.db("TO_DO_DB").collection("Tasks");
-        collection.insertOne(req.body).then(()=>{
-            res.send(req.body);
-            res.end();
-            // client.close();
-        });
+        let task = req.body;
+        console.log(req.body);
+        if(checkSchema(task,Task)) {
+            collection.insertOne(task).then(()=>{
+                res.status(200);
+                res.send(task);
+            });
+        } else {
+            res.status(400);
+            res.send("Schema mismatch");
+        }
+        res.end();
     });
 });
 
-
 app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
+
+//check if testObj has correct schema
+function checkSchema(testObj,correctObj){
+    for(let key of Object.keys(correctObj)){
+        if(!testObj.hasOwnProperty(key)) return false;
+    }
+    return true;
+}
